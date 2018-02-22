f7a737a866a9dfab17c6842b11c91fb20a39f34b
user <user@user-PC20>
2018-02-22 17:49:52 +0900

    오류수정

--- sevenget/src/main/java/com/sevenget/seven/HomeController.java
+++ sevenget/src/main/java/com/sevenget/seven/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sevenget.makeplot.MakingPlot;
+import com.sevenget.Rcode.MakingPlot;
 
 import model.member.MemIdCheckDaoImpl;
 
