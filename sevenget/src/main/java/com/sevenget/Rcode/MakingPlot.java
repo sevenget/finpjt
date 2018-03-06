afc6aaea296ee96f28ba2fc98ca613cf86c57fe3
user <user@user-PC8>
2018-03-06 11:58:45 +0900

    0.05

--- sevenget/src/main/java/com/sevenget/Rcode/MakingPlot.java
+++ sevenget/src/main/java/com/sevenget/Rcode/MakingPlot.java
@@ -39,8 +39,8 @@ public class MakingPlot {
 		connection.eval("data=rbind(rep(10,7) , rep(0,7) , data)"); //DB에서 받아온 데이터 삽입 구간. 회사와 사용자의 7가지 점수가 들어갈 예정.
 		
 		// plot 그래픽 설정 및 파일 저장
-		connection.eval("colors_border=c(rgb(173/255, 215/255, 46/255,0.9), rgb(101/255, 78/255, 163/255,0.9))");
-		connection.eval("colors_in=c( rgb(193/255, 239/255, 56/255,0.4), rgb(195/255, 188/255, 210/255,0.4))");
+		connection.eval("colors_border=c(rgb(0/255, 0/255, 0/255,0.9), rgb(101/255, 78/255, 163/255,0.9))");
+		connection.eval("colors_in=c( rgb(0/255, 0/255, 0/255,0.05), rgb(195/255, 188/255, 210/255,0.9))");
 		connection.eval("setwd('C:/Users/user/git/finpjt/sevenget/src/main/webapp/resources/img/plots')"); // 저장 장소 설정
 		
 		System.out.println("=====================");
@@ -51,7 +51,7 @@ public class MakingPlot {
 		
 		connection.eval("png(filename = 'radarchart"+CScoDto.getCid()+"_"+id+".png', width = 510, height = 400)"); // plot의 너비와 높이는 언제든지 변경가능!
 		connection.eval("par(mar=c(1,1,1,1))");
-		connection.eval("radarchart( data  , axistype=1 , seg = 2, pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1, cglcol='grey', cglty=1, axislabcol='grey', caxislabels=seq(0,10,5), cglwd=0.8,vlcex=0.8)");
+		connection.eval("radarchart( data  , axistype=1 , seg = 2, pcol=colors_border , pfcol=colors_in , plwd=c(1,4), plty=c(3,1), cglcol='grey', cglty=1, axislabcol='grey', caxislabels=seq(0,10,5), cglwd=0.8,vlcex=0.8)");
 		connection.eval("legend(x=0.7, y=1.3, legend = rownames(data[-c(1,2),]), bty = 'n', pch=20 , col=colors_in , text.col = 'grey', cex=1.2, pt.cex=3)");
 		connection.eval("dev.off ()");
 		System.out.println("=========================");
